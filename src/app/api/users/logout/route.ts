e53dc5a792cfe5cd95205d0d644bbf05492ed429
Stebinraj <stebinraj22@gmail.com>
2023-08-12 10:10:44 +0530

    ..

--- src/app/api/users/logout/route.ts
+++ src/app/api/users/logout/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 export const GET = () => {
     try {
         const response = NextResponse.json({ message: 'Logout Succcessfully' });
-            response.cookies.set('token', '', { expires: new Date(0) });
+        response.cookies.set('token', '', { expires: new Date(0) });
         return response;
     } catch (error: any) {
         return NextResponse.json({ message: error.message }, { status: 500 });
