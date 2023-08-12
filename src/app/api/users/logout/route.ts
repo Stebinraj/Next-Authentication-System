4ca71816b8133e19997f7dd9b19ab63ca5cd8b84
Stebinraj <stebinraj22@gmail.com>
2023-08-12 10:10:32 +0530

    ..

--- src/app/api/users/logout/route.ts
+++ src/app/api/users/logout/route.ts
@@ -1,12 +1,9 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
-export const GET = (request: NextRequest) => {
+export const GET = () => {
     try {
-        const cookies = request.cookies.getAll();
         const response = NextResponse.json({ message: 'Logout Succcessfully' });
-        for (const i of cookies) {
-            response.cookies.set(i.name, '', { expires: new Date(0) });
-        }
+            response.cookies.set('token', '', { expires: new Date(0) });
         return response;
     } catch (error: any) {
         return NextResponse.json({ message: error.message }, { status: 500 });
